8b5007b9806d454da6c453e62e7b2480c21cdede
easylearningbd <elebdsoft@gmail.com>
2024-11-24 04:41:39 +0600

    first commit

---
+++ components/Text.jsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+import { Button } from './ui/button';
+import { toast } from 'sonner';
+
+const Text = () => {
+
+    const handleClick = (mode) => {
+        mode ? toast.success('Test Success') : toast.error("test error");
+    }
+
+    return (
+        <div>
+ <Button className="bg-red-600" variant="default"
+              onClick={() => handleClick(false) } >Hello Ariyan </Button>
+        </div>
+    );
+};
+
+export default Text;
