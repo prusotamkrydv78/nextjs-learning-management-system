41a2261ebe6739b20c803c188fc4221c24a771a1
easylearningbd <elebdsoft@gmail.com>
2024-11-25 02:15:14 +0600

    s12 Model Creation and Query Testing Part 4

--- queries/courses.js
+++ queries/courses.js
@@ -1,6 +1,14 @@
+import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
+import { User } from "@/model/user-model";
 
 export async function getCourses() {
-    const courses = await Course.find({});
+    const courses = await Course.find({}).populate({
+        path: "category",
+        model: Category
+    }).populate({
+        path: "instructor",
+        model: User
+    });
     return courses;
 } 
