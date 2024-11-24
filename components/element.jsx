bf70d2ae9b3cde657eba24a3e31559c015901714
easylearningbd <elebdsoft@gmail.com>
2024-11-25 04:36:26 +0600

    s17 Design Home Page Part 3

--- components/element.jsx
+++ components/element.jsx
@@ -21,6 +21,24 @@ const element = () => {
         /> 
     </div> 
     </div>
+
+    <div className='w-full bg-blue-50 p-6 flex flex-col md:flex-row items-center pt-5 pb-10 pl-10'>
+    <div className='md:w-1/2 flex justify-center mb-6 md:mb-0'>
+    <Image
+            src="/assets/images/one.png"
+            alt='Put Your Learning'
+            width={500}
+            height={400}
+            className='rounded-lg' 
+        />  
+    </div>
+
+    <div className='md:w-1/2 text-center md:text-left'>
+        <h3 className='text-green-600 font-semibold text-lg mb-2'>Step-by-step lessons</h3>
+        <h2 className='text-gray-800 font-bold text-5xl mb-4'>Put Your Learning <br/> Into Practice</h2>
+        <p className='text-gray-600'>Apply your learning with real-world projects and learn everything you need to take your career to the next level.</p>
+        </div>  
+    </div> 
     
 </div>
     );
