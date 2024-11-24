29c0b78d6114975ba4599faed26266bd9a21e0a3
easylearningbd <elebdsoft@gmail.com>
2024-11-25 03:56:21 +0600

    s15 Design Home Page Part 1

---
+++ app/(main)/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainLayout = ({children}) => {
+    return (
+        <div>
+            
+            <main> {children} </main>
+        </div>
+    );
+};
+
+export default MainLayout;
