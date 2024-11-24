fdd11d25269169ecaab9a5c4a47a626487d81377
easylearningbd <elebdsoft@gmail.com>
2024-11-24 22:46:37 +0600

    s11 Model Creation and Query Testing Part 3

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 import Text from "@/components/Text"; 
+import { getCourses } from "@/queries/courses";
 
-export default function Home() {
+export default async function Home() {
+
+  const courses = await getCourses();
+  console.log(courses);
+
+
+
   return (
     <Text/>
   );
