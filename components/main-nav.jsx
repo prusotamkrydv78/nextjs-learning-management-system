a3d528c340fd6468b46976d5119cf0b633b622db
easylearningbd <elebdsoft@gmail.com>
2024-11-26 00:51:52 +0600

    s22 Design Home Page Part 8

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -7,7 +7,7 @@ import { X } from 'lucide-react';
 import { Button, buttonVariants } from './ui/button';
 import { Menu } from 'lucide-react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'; 
 
 const MainNav = ({items,children}) => {
     return (
@@ -36,9 +36,57 @@ const MainNav = ({items,children}) => {
      
 </div>
 
-    <nav>
-    login
+    <nav className='flex items-center gap-3'>
+        <div className='items-center gap-3 hidden lg:flex'>
+            <Link href='/login' className={cn(buttonVariants({size: "sm"}), "px-4")}>
+                Login
+            </Link>
+<DropdownMenu>
+    <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="sm">Register</Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end" className="w-56 mt-4">
+        <DropdownMenuItem className="cursor-pointer">
+            <Link href=''>Student</Link> 
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer">
+            <Link href=''>Instructor</Link> 
+        </DropdownMenuItem> 
+    </DropdownMenuContent>  
+</DropdownMenu> 
+        </div>
 
+
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <div className='cursor-pointer'>
+    <Avatar>
+    <AvatarImage src="https://github.com/shadcn.png" alt="@ariyan" />
+    <AvatarFallback>CN</AvatarFallback> 
+    </Avatar>
+            </div> 
+     </DropdownMenuTrigger>
+
+     <DropdownMenuContent align="end" className="w-56 mt-4">
+        <DropdownMenuItem className="cursor-pointer" asChild>
+            <Link href='account'>Profile</Link> 
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer" asChild>
+            <Link href='account/enrolled-courses'>My Courses</Link> 
+        </DropdownMenuItem> 
+        <DropdownMenuItem className="cursor-pointer" asChild>
+            <Link href=''>Testimonials & Certificates</Link> 
+        </DropdownMenuItem> 
+        <DropdownMenuItem className="cursor-pointer" asChild>
+            <Link href=''>Logout</Link> 
+        </DropdownMenuItem> 
+    </DropdownMenuContent>  
+
+
+    </DropdownMenu>
+
+
+    
     </nav> 
 
 </>
