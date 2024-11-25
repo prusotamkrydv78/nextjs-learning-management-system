66ac6f8978a6e050c2d963ae332327e6bc0727d8
easylearningbd <elebdsoft@gmail.com>
2024-11-25 23:51:12 +0600

    s21 Design Home Page Part 7

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,6 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
 import Logo from './logo';
+import { cn } from '@/lib/utils';
+
+import { X } from 'lucide-react';
+import { Button, buttonVariants } from './ui/button';
+import { Menu } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const MainNav = ({items,children}) => {
     return (
@@ -9,8 +16,31 @@ const MainNav = ({items,children}) => {
     <Link href="/">
         <Logo/>
     </Link>
-    menu 
+    {
+        items?.length ? (
+            <nav className='hidden gap-6 lg:flex'>
+                {
+                   items?.map((item,index) => (
+                    <Link
+                    key={index}
+                    href={item.disable ? "#" : item.href}
+                    className={cn("flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm ")}
+                    >
+                    {item.title}
+                    </Link>
+                   )) 
+                }
+            </nav> 
+        ) : null
+    }
+     
 </div>
+
+    <nav>
+    login
+
+    </nav> 
+
 </>
 
 
