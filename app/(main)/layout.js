faa0ba88b8c577fcec8e91677eaf4a96c06c2bf2
easylearningbd <elebdsoft@gmail.com>
2024-11-26 05:32:53 +0600

    s28 Design and Get Course List Page Data Part 1

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -32,7 +32,7 @@ const MainLayout = ({children}) => {
 
     </header>
     
-    <main> {children} </main>
+    <main className='flex-1 pt-20 flex flex-col'> {children} </main>
     <SiteFooter/>
 
 </div>
