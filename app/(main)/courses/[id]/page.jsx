49453d86e71edfba6dbb7627f32229cc461b33a5
easylearningbd <elebdsoft@gmail.com>
2024-11-27 05:44:49 +0600

    s40 Get Data From Database in Course Details Page Part 5

--- app/(main)/courses/[id]/page.jsx
+++ app/(main)/courses/[id]/page.jsx
@@ -20,7 +20,7 @@ const SingleCoursePage = async ({ params: {id} }) => {
         thumbnail={course?.thumbnail}
       />
 
-      <CourseDetails/>
+      <CourseDetails course={course} />
     {
       course?.testimonials && <Testimonials testimonials={replaceMongoIdInArray(course?.testimonials)} />   
     }
