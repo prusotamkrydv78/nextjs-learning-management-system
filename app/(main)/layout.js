a6d4087043f6d9bf0dd7aea3ebc5b02cac03d141
easylearningbd <elebdsoft@gmail.com>
2024-11-29 01:55:13 +0600

    s54 Login Logout Instructor and Student with Next Auth Part 4

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,6 +1,7 @@
 import MainNav from '@/components/main-nav';
 import SiteFooter from '@/components/site-footer';
 import React from 'react';
+import { SessionProvider } from "next-auth/react"
 
 const navLinks = [
     { 
@@ -26,9 +27,12 @@ const MainLayout = ({children}) => {
     return ( 
 <div className='flex min-h-screen flex-col'>
     <header className='z-40 bg-background/60 backdrop-blur-md fixed top-0 left-0 right-0 border-b'>
+    
+    <SessionProvider>
     <div className='container flex h-20 items-center justify-between py-6'>
         <MainNav items={navLinks} />
     </div>
+    </SessionProvider>
 
     </header>
     
--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { User } from "./model/user-model";
 import bcrypt from 'bcryptjs';
-
+ 
 export const {
     handlers: {GET, POST},
     auth,
