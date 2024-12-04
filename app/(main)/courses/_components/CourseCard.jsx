92ee221d306fb7b55ce205cf8ed93941829accaf
easylearningbd <elebdsoft@gmail.com>
2024-12-05 01:54:34 +0600

    s60 Stripe Integration Part 1 .

--- app/(main)/courses/_components/CourseCard.jsx
+++ app/(main)/courses/_components/CourseCard.jsx
@@ -6,7 +6,8 @@ import { ArrowRight } from "lucide-react";
 import { formatPrice } from "@/lib/formatPrice";
 import { ArrowRightIcon } from "lucide-react";
 import { BookOpen } from "lucide-react";
-
+import EnrollCourse from '@/components/enroll-course';
+ 
 const CourseCard = ({course}) => {
     return (
         <Link key={course.id} href={`/courses/${course.id}`}>
@@ -44,13 +45,7 @@ const CourseCard = ({course}) => {
                 {formatPrice(course?.price)}
               </p>
 
-              <Button
-                variant="ghost"
-                className="text-xs text-sky-700 h-7 gap-1"
-              >
-                Enroll
-                <ArrowRight className="w-3" />
-              </Button>
+            <EnrollCourse asLink={true} />
             </div>
           </div>
         </div>
