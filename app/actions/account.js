318fc0e7409f9d531ccdd142bfd79a06ea6d18e0
easylearningbd <elebdsoft@gmail.com>
2024-12-07 05:55:55 +0600

    s77 Reset Password and Account Update Part 7  .

--- app/actions/account.js
+++ app/actions/account.js
@@ -1,7 +1,9 @@
 "use server"
 
 import { User } from "@/model/user-model";
-import { revalidatePath } from "next/cache";
+import { validatePassword } from "@/queries/users";
+import { revalidatePath } from "next/cache"; 
+import bcrypt from 'bcryptjs';
 
 export async function updateUserInfo(email,updatedData){
     try {
@@ -16,5 +18,23 @@ export async function updateUserInfo(email,updatedData){
 // End method 
 
 export async function changePassword(email, oldPassword, newPassword) {
+    const isMatch = await validatePassword(email,oldPassword);
     
+    if (!isMatch) {
+        throw new Error("Please enter a valid current password");        
+    }
+    const filter = {email: email};
+    const hashedPassword = await bcrypt.hash(newPassword, 5);
+
+    const dataToUpadate ={
+        password: hashedPassword
+    };
+
+    try { 
+        await User.findOneAndUpdate(filter,dataToUpadate);
+        revalidatePath('/account');
+    } catch (error) {
+        throw new Error(error);
+    } 
+
 }
