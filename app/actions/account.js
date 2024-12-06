7799df44a1f6fd2c54b74d77868625ba53f2251c
easylearningbd <elebdsoft@gmail.com>
2024-12-07 04:47:51 +0600

    s75 Reset Password and Account Update Part 5 .

--- app/actions/account.js
+++ app/actions/account.js
@@ -1,6 +1,16 @@
 "use server"
 
+import { User } from "@/model/user-model";
+import { revalidatePath } from "next/cache";
+
 export async function updateUserInfo(email,updatedData){
+    try {
+        const filter = {email: email};
+        await User.findOneAndUpdate(filter,updatedData);
+        revalidatePath('/account');
+    } catch (error) {
+        throw new Error(error);
+    }
 
 }
 // End method 
