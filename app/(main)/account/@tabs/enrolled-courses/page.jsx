27b37ee2aa34bc09bbca405a197a75752c47391f
easylearningbd <elebdsoft@gmail.com>
2024-12-08 00:13:15 +0600

    s80 Enrolled Course Details, Calculations Part 2 .

--- app/(main)/account/@tabs/enrolled-courses/page.jsx
+++ app/(main)/account/@tabs/enrolled-courses/page.jsx
@@ -15,14 +15,26 @@ async function EnrolledCourses() {
 	}
 
 	const loggedInUser = await getUserByEmail(session?.user?.email);
-	console.log(loggedInUser);
-
+	 
 	const enrollments = await getEnrollmentsForUser(loggedInUser?.id)
+	console.log(enrollments);
 
 	return (
-		<div className="grid sm:grid-cols-2 gap-6">
-			 <EnrolledCourseCard/>
-		</div>
+	<div className="grid sm:grid-cols-2 gap-6">
+		{
+			enrollments && enrollments.length > 0 ? (
+				<>
+				{ enrollments.map((enrollment) => (
+					<EnrolledCourseCard key={enrollment?.id} enrollment={enrollment}  />
+				))}
+				</>
+
+			) : (
+				<p className="font-bold text-red-700">No Enrollments found!</p>
+			)
+		}
+			
+	</div>
 	);
 }
 
--- queries/enrollments.js
+++ queries/enrollments.js
@@ -1,4 +1,5 @@
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
+import { Course } from "@/model/course-model";
 import { Enrollment } from "@/model/enrollment-model"; 
 
 export async function getEnrollmentsForCourse(courseId){
@@ -23,5 +24,15 @@ export async function enrollForCourse(courseId, userId, paymentMethod){
 }
 
 export async function getEnrollmentsForUser(userId){
-    
+    try {
+        const enrollments = await Enrollment.find({ student: userId})
+        .populate({
+            path: "course",
+            model: Course,
+        }).lean();
+        return replaceMongoIdInArray(enrollments);
+    } catch (err) {
+        throw new Error(err);
+    }
+
 }
