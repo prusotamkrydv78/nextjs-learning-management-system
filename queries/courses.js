d9391f4321f1af39c88363f73627a713624e0f72
easylearningbd <elebdsoft@gmail.com>
2024-12-09 01:46:39 +0600

    s91 Dashboard - Features, Analytics, Profile, Logout Part 3.

--- queries/courses.js
+++ queries/courses.js
@@ -47,6 +47,19 @@ export async function getCourseDetails(id) {
     return replaceMongoIdInObject(course);
 }  
 
+
+function groupBy(array, keyFn){
+    return array.reduce((acc, item) => {
+        const key = keyFn(item);
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(item);
+        return acc
+    },{});
+}
+
+
 export async function getCourseDetailsByInstructor(instructorId){
     const courses = await Course.find({instructor: instructorId })
     .populate({path: "category", model: Category })
@@ -60,6 +73,19 @@ export async function getCourseDetailsByInstructor(instructorId){
                 return enrollment;
         })
     );
+
+   
+
+    // Group enrollments by course
+    const groupByCourses = groupBy(enrollments.flat(), (item) => item.course);
+
+    /// Calculate total revenue 
+    const totalRevenue = courses.reduce((acc, course) => {
+        const enrollmentsForCourse = groupByCourses[course._id] || [];
+        return acc + enrollmentsForCourse.length * course.price; 
+    },0);
+
+    //console.log(totalRevenue);
 
     const totalEnrollments = enrollments.reduce(( acc, obj )=> {
         return acc + obj.length;
@@ -96,6 +122,7 @@ export async function getCourseDetailsByInstructor(instructorId){
         "reviews" : totalTestimonials.length,
         "ratings" : avgRating.toPrecision(2),
         "inscourses" : courses,
+        "revenue": totalRevenue,
         fullInsName,
         Designation,
         insImage
