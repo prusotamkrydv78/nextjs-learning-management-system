6a151ea983dafe02ee04812dea5a563b7349675a
easylearningbd <elebdsoft@gmail.com>
2024-12-13 20:45:05 +0600

    s113 Course Create, Meta Data Updates, Image Upload Part 9

--- app/dashboard/courses/[courseId]/page.jsx
+++ app/dashboard/courses/[courseId]/page.jsx
@@ -64,7 +64,7 @@ const EditCourse = async ({ params: {courseId} }) => {
             />
             <DescriptionForm initialData={{description: course?.description }} courseId={courseId} />
 
-            <ImageForm initialData={{}} courseId={1} />
+            <ImageForm initialData={{imageUrl: `/assets/images/courses/${course?.thumbnail}`}} courseId={courseId} />
            
             <CategoryForm initialData={{value: course?.category?.title }} courseId={courseId} options={mappedCategories} />
 
