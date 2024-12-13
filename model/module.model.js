cdb70400a7341789a9b4050b825ed7bcfb9eb225
easylearningbd <elebdsoft@gmail.com>
2024-12-13 22:02:32 +0600

    s116 Module Creation, Reordering of Modules Part 1

--- model/module.model.js
+++ model/module.model.js
@@ -5,13 +5,13 @@ const moduleSchema = new Schema({
         required: true,
         type: String
     },
-    description:{
-        required: true,
+    description:{ 
         type: String
     },
-    status:{
+    active:{
         required: true,
-        type: String
+        default: false,
+        type: Boolean
     },
     slug:{
         required: true,
@@ -19,12 +19,16 @@ const moduleSchema = new Schema({
     },
     course:{
         required: true,
-        type: String
+        type: Schema.ObjectId,
     },
     lessonIds:{
         required: true,
-        type: [String]
+        type: [Schema.ObjectId]
     },
+    order:{
+        require: true,
+        type: Number
+    }
      
 });
 export const Module = mongoose.models.Module ?? mongoose.model("Module",moduleSchema);
