4f0ac3667cfcae260cd093e94879baa5a471fbf7
easylearningbd <elebdsoft@gmail.com>
2024-12-19 04:02:41 +0600

    s146 Create Course Quiz Option Part 6

--- app/dashboard/quiz-sets/[quizSetId]/page.jsx
+++ app/dashboard/quiz-sets/[quizSetId]/page.jsx
@@ -15,52 +15,7 @@ import { CircleCheck } from "lucide-react";
 import { Circle } from "lucide-react";
 import { getQuizSetById } from "@/queries/quizzes";
 
-// const initialQuizes = [
-//   {
-//     id: 1,
-//     title: "What is HTML ?",
-//     options: [
-//       {
-//         label: "A programming language",
-//         isTrue: false,
-//       },
-//       {
-//         label: "A markup language",
-//         isTrue: true,
-//       },
-//       {
-//         label: "A famous book",
-//         isTrue: false,
-//       },
-//       {
-//         label: "A famous tv show",
-//         isTrue: false,
-//       },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     title: "What is Javascript ?",
-//     options: [
-//       {
-//         label: "A programming language",
-//         isTrue: true,
-//       },
-//       {
-//         label: "A markup language",
-//         isTrue: false,
-//       },
-//       {
-//         label: "A famous book",
-//         isTrue: false,
-//       },
-//       {
-//         label: "A famous tv show",
-//         isTrue: false,
-//       },
-//     ],
-//   },
-// ]; 
+ 
 const EditQuizSet = async ({ params: {quizSetId} }) => {
 
   const quizSet = await getQuizSetById(quizSetId);
@@ -154,9 +109,7 @@ const EditQuizSet = async ({ params: {quizSetId} }) => {
             </div>
             <div className="max-w-[800px]">
               <TitleForm
-                initialData={{
-                  title: "Reactive Accelerator",
-                }}
+                initialData={{ title: quizSet.title  }} quizSetId={quizSetId}
               />
             </div>
 
