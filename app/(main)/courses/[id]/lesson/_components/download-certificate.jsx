8f3e8452203a9dcdc2d805fb2a7d3b00d45ee2b0
easylearningbd <elebdsoft@gmail.com>
2025-01-03 02:08:12 +0600

    s188 Course Certificate PDF Download Option Part 2

--- app/(main)/courses/[id]/lesson/_components/download-certificate.jsx
+++ app/(main)/courses/[id]/lesson/_components/download-certificate.jsx
@@ -1,9 +1,38 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export const DownloadCertificate = ({courseId,totalProgress}) => {
 
+    const [isCertificateDownloading,setIsCertificateDownloading] = useState(false);
+
+    async function handleCertificateDownload(){
+        try {
+            setIsCertificateDownloading(true);
+            fetch(`/api/certificate?courseId=${courseId}`)
+            .then((respone) => respone.blob())
+            .then((blob)=> {
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "Certificate.pdf";
+                document.body.appendChild(a); 
+                a.click();
+                a.remove(); 
+            })
+            toast.success("Cerificate has been downloaded"); 
+
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setIsCertificateDownloading(false);
+        }
+    }
+
     return (
         <Button
+        onClick={handleCertificateDownload}
          disabled={totalProgress < 100}
          className="w-full mt-6">
            Download Certificate 
