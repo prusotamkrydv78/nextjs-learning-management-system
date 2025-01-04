59f9cd65592fc5e37731e06d23b4abad99058557
easylearningbd <elebdsoft@gmail.com>
2025-01-04 21:12:28 +0600

    s196 Manage Quiz Submission for Course Part 7

--- app/actions/quiz.js
+++ app/actions/quiz.js
@@ -1,8 +1,11 @@
 "use server"
 import { Quizset } from "@/model/quizset-model";
-import { getSlug } from './../../lib/convertData';
-import { createQuiz } from "@/queries/quizzes";
+import { getSlug, replaceMongoIdInArray } from './../../lib/convertData';
+import { createQuiz, getQuizSetById } from "@/queries/quizzes";
 import { Quiz } from "@/model/quizzes-model";
+import mongoose from "mongoose";
+import { Assessment } from "@/model/assessment-model";
+import { getLoggedInUser } from "@/lib/loggedin-user";
 
 
 export async function updateQuizSet(quizset, dataToUpdate){
@@ -89,5 +92,50 @@ export async function doCreateQuizSet(data){
 }
 
 export async function addQuizAssessment(courseId,quizSetId,answers) {
+    try {
+        console.log(quizSetId,answers);
+        const quizSet = await getQuizSetById(quizSetId);
+        const quizzes = replaceMongoIdInArray(quizSet.quizIds);
+
+        const assessmentRecord = quizzes.map((quiz) => {
+            const obj = {};
+            obj.quizId = new mongoose.Types.ObjectId(quiz.id);
+            const found = answers.find((a) => a.quizId === quiz.id);
+            if (found) {
+                obj.attempted = true;
+            } else {
+                obj.attempted = false;
+            }
+
+        const mergedOptions = quiz.options.map((o) => {
+            return {
+                option: o.text,
+                isCorrect: o.is_correct, 
+                isSelected: (function () {
+                    const found = answers.find((a) => a.options[0].option === o.text);
+                    if (found) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })(),
+            };
+        }); 
+        
+        obj["options"] = mergedOptions;
+        return obj;  
+      });
+
+      const assessmentEntry = {};
+      assessmentEntry.assessments = assessmentRecord;
+      assessmentEntry.otherMarks = 0;
+
+      const assessment = await Assessment.create(assessmentEntry);
+      const loggedInUser = await getLoggedInUser();
+      
+
+    } catch (error) {
+        
+    }
 
 }
