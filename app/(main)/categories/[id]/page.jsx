e26825d67ac532c4ff48ccb5d5e0a1d4b0bc1bcb
easylearningbd <elebdsoft@gmail.com>
2025-01-05 01:59:51 +0600

    s202 Get Category Wise Courses Part 1

---
+++ app/(main)/categories/[id]/page.jsx
@@ -0,0 +1,40 @@
+ 
+ 
+import { getCourseList } from "@/queries/courses"; 
+import CourseCard from "../../courses/_components/CourseCard";
+
+ 
+const CoursesCatgoryPage = async () => {
+    
+  const courses = await getCourseList();
+  
+
+  return (
+    <section
+      id="courses"
+      className="container space-y-6   dark:bg-transparent py-6"
+    >
+      
+      <div className="flex items-baseline justify-between  border-gray-200 border-b pb-6 flex-col gap-4 lg:flex-row">
+        Category Name 
+        
+      </div>
+      
+ 
+    
+      <section className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4"> 
+         
+          <div className="lg:col-span-4 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-3 gap-4">
+            {courses.map((course) => {
+              return (
+                <CourseCard key={course.id} course={course} />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+export default CoursesCatgoryPage;
